e23524633d9666260d5db5553069623d377c4ba4
Mohammed Abdul Mannan <mannan77980@gmail.com>
2020-05-14 21:47:05 +0530

    Changed Background, NavBar and added Scroll Nav effect

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,41 @@ $(window).on('load', function () {
 });
 */
 
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/csi-black.png");
+
+            // Show back to top button
+            /*$("#back-to-top").fadeIn();*/
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/csi-black.png");
+
+            // Hide back to top button
+            /*$("#back-to-top").fadeOut();*/
+        }
+    }
+});
